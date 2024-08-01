5ea4a640619923b32eb7e6c610bd67493332f248
Thierry Berger <contact@thierryberger.com>
2024-08-01 10:26:10 +0200

    exact same parameters as original bug + adapt test

--- crates/parry2d/tests/geometry/epa_convergence.rs
+++ crates/parry2d/tests/geometry/epa_convergence.rs
@@ -1,15 +1,14 @@
 use na::Vector2;
 use parry2d::{
-    math::{Isometry, Point, Real, Vector},
+    math::{Isometry, Point, Real},
     query,
-    shape::{Capsule, ConvexPolygon},
+    shape::{Capsule, ConvexPolygon, SharedShape},
 };
 
+/// Original issue: https://github.com/dimforge/parry/issues/205
 #[test]
 fn capsule_convergence() {
-    let p = Point::from(Vector::y() * 5.0);
-
-    let shape1 = Capsule::new(-p, p, 10.0);
+    let shape1 = Capsule::new_y(5.0, 10.0);
     let mut vec = Vec::<Point<Real>>::with_capacity(3);
     vec.push(Point::<Real>::new(64.0, 507.0));
     vec.push(Point::<Real>::new(440.0, 326.0));
@@ -19,17 +18,25 @@ fn capsule_convergence() {
     let transform1 = Isometry::new(Vector2::new(381.592, 348.491), 0.0);
     let transform2 = Isometry::new(Vector2::new(0.0, 0.0), 0.0);
 
-    let res = query::details::contact_support_map_support_map(
+    let _res = query::details::contact_support_map_support_map(
         &transform1.inv_mul(&transform2),
         &shape1,
         &shape2,
         10.0,
     )
     .expect("Penetration not found.");
+    let shared_shape1 = SharedShape::new(shape1);
+    let shared_shape2 = SharedShape::new(shape2);
 
-    if let Ok(Some(contact)) = query::contact(&transform1, &shape1, &transform2, &shape2, 1.0) {
-        panic!("collision");
+    if let Ok(Some(_contact)) = query::contact(
+        &transform1,
+        shared_shape1.as_ref(),
+        &transform2,
+        shared_shape2.as_ref(),
+        1.0,
+    ) {
+        println!("collision");
     } else {
-        print!("no collision");
+        panic!("no collision");
     }
 }
